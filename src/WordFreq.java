80c0ec36268ed83e0072a41b2cb0b7f5a656179a
Pouya Anvari <your-email@example.com>
2026-02-14 12:28:00 -0800

    ADD: WordFreq constructor

---
+++ src/WordFreq.java
@@ -0,0 +1,17 @@
+
+
+    /* You are given a list of commonly used customer‑survey phrases, 
+    each paired with the number of times it appeared in recent feedback. */
+public class WordFreq {
+
+    String word; //phrase
+    int frequency; //number of times phrase appeared: frequency determines priority (higher frequency --> higher priority)
+
+    public WordFreq(String word, int frequency){
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+
+
+}
